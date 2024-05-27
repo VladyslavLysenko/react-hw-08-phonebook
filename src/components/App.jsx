0c1eb9960deef7874464a691a82c92c4a7db1b46
Nancy311 <105671987+VladyslavLysenko@users.noreply.github.com>
2024-05-27 13:22:26 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Form } from './Form';
+import { ContactsList } from './ContactsList';
+import Filter from './Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from './redux/operations';
+import { getError, getIsLoading } from './redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Form />
+      <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <ContactsList />
+    </>
   );
 };
